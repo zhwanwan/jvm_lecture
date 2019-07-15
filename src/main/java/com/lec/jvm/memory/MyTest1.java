13f083f9764a0173e10ab176e3adfc72bd984733
zhwanwan <334271937@qq.com>
2019-07-16 07:55:50 +0800

    jvm

---
+++ src/main/java/com/lec/jvm/memory/MyTest1.java
@@ -0,0 +1,16 @@
+package com.lec.jvm.memory;
+
+/**
+ *  JVM内存空间：
+ *  1.虚拟机栈：Stack Frame
+ *  2.程序计数器：Program Counter
+ *  3.本地方法栈：执行本地方法
+ *  4.堆（Heap）：JVM管理的最大一块空间
+ *  5.方法区（Method Area)：存储元信息。永久代（Permanent Generation），从JDK1.8开始，已经彻底废弃永久代。使用元空间（meta space)。
+ *  6.运行时常量池：方法区的一部分内容，编译期间使用。
+ *  7.直接内存：Direct Memory（Netty中的堆外内存）
+ *
+ */
+public class MyTest1 {
+
+}
